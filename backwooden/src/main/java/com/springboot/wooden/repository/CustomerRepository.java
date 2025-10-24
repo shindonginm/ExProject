742738f6711ff47ef4cc2c42b6c821dba46ff75e
dongin <azide98@naver.com>
2025-10-25 00:02:11 +0900

    리포지토리 수정

--- backwooden/src/main/java/com/springboot/wooden/repository/CustomerRepository.java
+++ backwooden/src/main/java/com/springboot/wooden/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.springboot.wooden.repository;
 import com.springboot.wooden.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
--- backwooden/src/main/java/com/springboot/wooden/repository/PartStockRepository.java
+++ backwooden/src/main/java/com/springboot/wooden/repository/PartStockRepository.java
@@ -1,10 +1,7 @@
-// backend/repository/PartStockRepository.java
 package com.springboot.wooden.repository;
 
 import com.springboot.wooden.domain.PartStock;
 import org.springframework.data.jpa.repository.*;
-import java.util.List;
-import java.util.Optional;
 
 public interface PartStockRepository extends JpaRepository<PartStock, Long> {
 
