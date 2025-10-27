63b585ae65f8a29ed17472a357b3f8b1eba4c26c
dongin <azide98@naver.com>
2025-10-27 16:12:29 +0900

    10/27발표전 코드

--- backwooden/src/main/java/com/springboot/wooden/repository/PartStockRepository.java
+++ backwooden/src/main/java/com/springboot/wooden/repository/PartStockRepository.java
@@ -1,7 +1,10 @@
+// backend/repository/PartStockRepository.java
 package com.springboot.wooden.repository;
 
 import com.springboot.wooden.domain.PartStock;
 import org.springframework.data.jpa.repository.*;
+import java.util.List;
+import java.util.Optional;
 
 public interface PartStockRepository extends JpaRepository<PartStock, Long> {
 
